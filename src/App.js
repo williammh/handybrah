06fef6d45e463116c934611fad824c53531fe825
William Huynh <whuynh23@gmail.com>
2019-11-09 16:59:04 -0800

    git deployment package.json

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          handybrah.com
         </a>
       </header>
     </div>
