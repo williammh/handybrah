cc09eeef7152a899d5a034e2d81b7f0d8460326e
William Huynh <whuynh23@gmail.com>
2019-11-09 17:48:07 -0800

    npx create-react-app

--- src/App.js
+++ src/App.js
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           handybrah under construction
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          handybrah.com
-        </a>
       </header>
     </div>
   );
