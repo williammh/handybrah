fb22a3dd5fad508699f6c5634d6c6b08e4ec8573
William Huynh <whuynh23@gmail.com>
2019-11-09 17:20:27 -0800

    Updates

---
+++ precache-manifest.08402c239a885378e5cd980888350436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19b8262a3af9850d756670ddb3d62fa",
+    "url": "/handybrah/index.html"
+  },
+  {
+    "revision": "aa72fdfcf275f84925f5",
+    "url": "/handybrah/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6786b477d5a44a6275aa",
+    "url": "/handybrah/static/js/2.7a75e02d.chunk.js"
+  },
+  {
+    "revision": "aa72fdfcf275f84925f5",
+    "url": "/handybrah/static/js/main.f8912064.chunk.js"
+  },
+  {
+    "revision": "7354e5d61fd8a09fc3e4",
+    "url": "/handybrah/static/js/runtime-main.bb1db13a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/handybrah/static/media/logo.25bf045c.svg"
+  }
+]);
