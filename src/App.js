8457db63cb79cb3278dbcb56e58c2c4693852b88
Cleonitus <rotomotocle@gmail.com>
2020-01-12 16:09:06 -0800

    Cleonscommit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           handybrah under construction
+           Cleon was header
         </p>
       </header>
     </div>
